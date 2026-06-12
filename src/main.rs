8c04bdb522047292907fc3b920cf0d7ab57ffad6
TOMINAGA HAYATO <tominaga_h@mad2007.co.jp>
2026-06-13 01:30:29 +0900

    CLI init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod commands;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "rscuff", version, about = "A scaffolding tool written by Rust")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Test,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Test => commands::test::run(),
+    }
 }
