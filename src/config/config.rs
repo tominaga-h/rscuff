fe60c24b927590edf1c7d6bab3f9046c3225fb07
TOMINAGA HAYATO <tominaga_h@mad2007.co.jp>
2026-06-14 21:13:11 +0900

    Config構造体の実装をとりあえず完了

--- src/config/config.rs
+++ src/config/config.rs
@@ -3,6 +3,7 @@ use camino::Utf8PathBuf;
 // 設定値を持つ構造体
 #[allow(dead_code)]
 pub struct Config {
+    // 設定フォルダの名称
     storage_name: String,
     // 設定フォルダを置く場所のパス。設定フォルダのパスは`<storage_path>/<storage_name>` というパスになる
     storage_dir: Utf8PathBuf,
@@ -16,14 +17,21 @@ impl Config {
         }
     }
 
+    // 設定フォルダのパスを返す
     pub fn get_storage_path(&self) -> Utf8PathBuf {
         self.storage_dir.join(&self.storage_name)
+    }
+
+    // 設定フォルダ内の `groups` フォルダのパスを返す
+    pub fn get_groups_path(&self) -> Utf8PathBuf {
+        self.get_storage_path().join("groups")
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use shellexpand;
     use tempfile::TempDir;
 
     #[test]
@@ -32,5 +40,26 @@ mod tests {
         let tempdir_path = Utf8PathBuf::from_path_buf(tempdir.path().to_path_buf()).unwrap();
         let config = Config::new(".rscuff".to_string(), tempdir_path.clone());
         assert_eq!(config.get_storage_path(), tempdir_path.join(".rscuff"));
+    }
+
+    #[test]
+    fn test_groups_path() {
+        let tempdir = TempDir::new().unwrap();
+        let tempdir_path = Utf8PathBuf::from_path_buf(tempdir.path().to_path_buf()).unwrap();
+        let config = Config::new(".rscuff".to_string(), tempdir_path.clone());
+        assert_eq!(
+            config.get_groups_path(),
+            tempdir_path.join(".rscuff").join("groups")
+        );
+    }
+
+    #[test]
+    fn test_groups_path_string() {
+        let homedir_path = Utf8PathBuf::from(shellexpand::tilde("~").to_string());
+        let config = Config::new(".rscuff".to_string(), homedir_path.clone());
+        assert_eq!(
+            config.get_groups_path().to_string(),
+            shellexpand::tilde("~/.rscuff/groups").to_string()
+        );
     }
 }
