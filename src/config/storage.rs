298b67eb3b37189d7721eae647d9670e5f6c1742
TOMINAGA HAYATO <tominaga_h@mad2007.co.jp>
2026-06-14 23:48:45 +0900

    ConfigStorage::createを実装

--- src/config/storage.rs
+++ src/config/storage.rs
@@ -1,4 +1,6 @@
 use crate::config::Config;
+use std::fs;
+use std::io::{Error, ErrorKind, Result};
 
 #[allow(dead_code)]
 pub struct ConfigStorage {
@@ -15,13 +17,28 @@ impl ConfigStorage {
     pub fn exists(&self) -> bool {
         self.config.get_storage_path().exists()
     }
+
+    // 設定フォルダを作成する。設定フォルダが既に存在する場合はエラーを返す
+    pub fn create(&self) -> Result<bool> {
+        if self.exists() {
+            return Err(Error::new(
+                ErrorKind::AlreadyExists,
+                "storage already exists",
+            ));
+        }
+
+        println!("storage: {}", self.config.get_storage_path());
+        match fs::create_dir(self.config.get_storage_path()) {
+            Ok(_) => Ok(true),
+            Err(e) => Err(e),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use camino::Utf8PathBuf;
-    use std::fs;
     use tempfile::TempDir;
 
     #[test]
@@ -38,9 +55,30 @@ mod tests {
         let tempdir = TempDir::new().unwrap();
         let tempdir_path = Utf8PathBuf::from_path_buf(tempdir.path().to_path_buf()).unwrap();
         let config = Config::new("test_rscuff".to_string(), tempdir_path);
-        fs::create_dir(config.get_storage_path()).unwrap();
         let storage = ConfigStorage::new(config);
+        storage.create().unwrap();
 
         assert!(storage.exists());
+    }
+
+    #[test]
+    fn test_create_true() {
+        let tempdir = TempDir::new().unwrap();
+        let tempdir_path = Utf8PathBuf::from_path_buf(tempdir.path().to_path_buf()).unwrap();
+        let config = Config::new("test_rscuff".to_string(), tempdir_path);
+        let storage = ConfigStorage::new(config);
+
+        assert!(storage.create().unwrap());
+    }
+
+    #[test]
+    fn test_create_error() {
+        let tempdir = TempDir::new().unwrap();
+        let tempdir_path = Utf8PathBuf::from_path_buf(tempdir.path().to_path_buf()).unwrap();
+        let config = Config::new("test_rscuff".to_string(), tempdir_path);
+        let storage = ConfigStorage::new(config);
+
+        storage.create().unwrap();
+        assert!(storage.create().is_err());
     }
 }
